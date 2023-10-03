0c6d0fef2bed90ee684991b3995044f158335077
slok35 <146804273+slok35@users.noreply.github.com>
2023-10-03 11:04:09 +0545

    Add files via upload

---
+++ food.js
@@ -0,0 +1,39 @@
+// const search_box = document.getElementById("search_box");
+// const search_btn = document.getElementById("search_icon");
+
+// search_btn.addEventListener("click",function(){
+//     search_box.classList.toggle('test');
+// })
+
+
+// let click= 1;
+
+
+
+
+
+// document.getElementById("search_box").style.visibility= "hidden";
+
+// if(click = 1)function act(){
+//     document.getElementById("search_box").style.visibility= "visible";
+
+
+// }else if(click +=1){
+//     document.getElementById("search_box").style.visibility= "hidden";
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
